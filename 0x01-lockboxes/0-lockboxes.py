ac0e71fe9f7f857a481d2e97d2387cdc767a19be
zaki-prod <zakiprod04@gmail.com>
2024-10-11 02:42:04 +0000

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-def canUnlockAll(boxes):
-    """
-    Determine if all the boxes can be opened.
-    
-    Args:
-        boxes (list): A list of lists where each inner list represents keys inside a box.
-        
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
-    """
-    n = len(boxes)  # Number of boxes
-    opened = [False] * n  # Track which boxes are opened
-    opened[0] = True  # The first box is always open
-    stack = [0]  # Start with box 0
-    
-    # Use DFS to visit all boxes we can open
-    while stack:
-        current_box = stack.pop()  # Take the current box to explore
-        for key in boxes[current_box]:
-            if key < n and not opened[key]:  # Valid key and box not opened yet
-                opened[key] = True  # Mark the box as opened
-                stack.append(key)  # Add the newly opened box to the stack
-    
-    # If all boxes are opened, return True
-    return all(opened)
 
+""" n number of boxes
+   each box is numbered sequentially from 0 to n - 1 and
+   each box may contain keys to the other boxes."""
+
+
+def canUnlockAll(boxes):
+    """ method that determines if all the boxes can be opened """
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
