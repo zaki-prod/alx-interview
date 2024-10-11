b7cdc60a503694169ab98395e38d8f3b6e95ef05
zaki-prod <zakiprod04@gmail.com>
2024-10-11 02:19:13 +0000

    0-lockboxes.py

---
+++ 0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Determines if all boxes can be unlocked using keys found in other boxes via DFS. 
+Returns True if all can be opened, otherwise False"""
+
+
+def canUnlockAll(boxes):
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
+    """
+
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
+        return True
+    return False
