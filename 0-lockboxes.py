6fc5f528c93d8a1b871cc096b24ea71539d89283
zaki-prod <zakiprod04@gmail.com>
2024-10-11 02:32:43 +0000

    0-lockboxes.py

--- 0-lockboxes.py
+++ 0-lockboxes.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Script will unlock list of lists"""
+
+""" n number of boxes
+   each box is numbered sequentially from 0 to n - 1 and
+   each box may contain keys to the other boxes."""
 
 
 def canUnlockAll(boxes):
-    """This function will take a list of lists and the content
-       of a list will unlock other lists
-    """
-
-    keys = [0]
-    for key in keys:
-        for boxKey in boxes[key]:
-            if boxKey not in keys and boxKey < len(boxes):
-                keys.append(boxKey)
-    if len(keys) == len(boxes):
+    """ method that determines if all the boxes can be opened """
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
         return True
     return False
