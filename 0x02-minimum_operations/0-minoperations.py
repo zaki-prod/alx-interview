054ef0cdb57c3fccdc6e1ef364cadd5673d3e224
zaki-prod <zakiprod04@gmail.com>
2024-10-15 17:06:35 +0000

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,29 @@
-
 #!/usr/bin/python3
+
 """
-Main file for testing
+    Method that determines the number of minmum operations given n characters
 """
 
-minOperations = __import__('0-minoperations').minOperations
 
-n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+def minOperations(n):
+    """
+        A function that calculates the fewest number of operations
+        needed to give a result of exactly n H characters in a file
+        args: n: Number of characters to be displayed
+        return:
+               number of min operations
+    """
 
-n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
+    now = 1
+    start = 0
+    counter = 0
+    while now < n:
+        remainder = n - now
+        if (remainder % now == 0):
+            start = now
+            now += start
+            counter += 2
+        else:
+            now += start
+            counter += 1
+    return counter
