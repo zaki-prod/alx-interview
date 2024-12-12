88db28ec9bfa3ada4c288f1fcedf870fd1ce7ee8
zaki-prod <zakiprod04@gmail.com>
2024-12-12 09:52:31 +0000

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,48 +1,29 @@
 #!/usr/bin/python3
-'''Prime Game'''
+""" Module for solving prime game question """
 
 def isWinner(x, nums):
-    '''Determines the overall winner of the game.'''
+    """function that checks for the winner"""
     if not nums or x < 1:
         return None
+    max_num = max(nums)
 
-    # Find the maximum number in nums to precompute primes
-    max_n = max(nums)
-    primes = sieve_of_eratosthenes(max_n)
-
-    # Precompute prime counts up to each number
-    prime_counts = [0] * (max_n + 1)
-    for i in range(1, max_n + 1):
-        prime_counts[i] = prime_counts[i - 1] + (1 if primes[i] else 0)
-
-    # Determine the winner for each round
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        if prime_counts[n] % 2 == 1:  # Odd count -> Maria wins
-            maria_wins += 1
-        else:  # Even count -> Ben wins
-            ben_wins += 1
-
-    # Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    my_filter = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not my_filter[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            my_filter[j] = False
+    my_filter[0] = my_filter[1] = False
+    y = 0
+    for i in range(len(my_filter)):
+        if my_filter[i]:
+            y += 1
+        my_filter[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += my_filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
         return None
-
-
-def sieve_of_eratosthenes(max_n):
-    '''Generates a list of primes up to max_n using the Sieve of Eratosthenes.'''
-    primes = [True] * (max_n + 1)
-    primes[0] = primes[1] = False  # 0 and 1 are not primes
-    for i in range(2, int(max_n**0.5) + 1):
-        if primes[i]:
-            for j in range(i * i, max_n + 1, i):
-                primes[j] = False
-    return primes
-if __name__ == "__main__":
-    print("Winner:", isWinner(5, [2, 5, 1, 4, 3]))
-
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
